365c638faa36df82f3c69eca8c1a045a2b923939
Gan <767214784@qq.com>
2017-05-05 13:19:35 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/wangbinying/android/db/Province.java
@@ -0,0 +1,41 @@
+package com.wangbinying.android.db;
+
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by wang on 2017/5/5 0005.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
