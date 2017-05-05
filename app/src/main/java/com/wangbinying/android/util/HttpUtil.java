5f01f7bee604f609279085d9e24a067ed6696546
Gan <767214784@qq.com>
2017-05-05 15:46:49 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/wangbinying/android/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.wangbinying.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by wang on 2017/5/5 0005.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
